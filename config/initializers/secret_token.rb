9de049f9addd82ae2297b4d74ff40dbe736ece84
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2011-07-06 20:12:37 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdcv::Application.config.secret_token = '050818584cf3615f1bfea6872badaae0ddfa66173ceaa2f3550c9550a524ac3d211923f09f9a72e1da897c949e570049628aed95bbc35bbdc1285706c908a584'
---
+++ public/javascripts/test/wdcv-test.js
@@ -0,0 +1,14 @@
+/*global alert, numberTest*/
+
+var wdcvTest = (function () {
+    function init() {                       
+        module("wdcv-test.js: init");        
+        test("ok", function () {            
+            ok(false);            
+        });                
+    }
+    
+    return {
+        init: init
+    };
+}());
