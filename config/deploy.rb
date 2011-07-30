9c1355e9d84c7e2ebb687172be88babdb6aaa441
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2011-07-30 15:46:01 +0400

    Changed ubuntu to www user in deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
-set :user, "ubuntu"
+set :user, "www"
 set :application, "wdcv"
 set :domain, 'morekot.ru'
 
 ssh_options[:forward_agent] = true
 
 set :scm, 'git'
-set :repository,  "ssh://ubuntu@morekot.ru/~/git/wdcv.git"
+set :repository,  "ssh://www@morekot.ru/~/git/wdcv.git"
 set :branch, "master"
 set :git_shallow_clone, 1
 
 
 # deploy config
-set :deploy_to, "/home/ubuntu/webdevelopercv.com"
+set :deploy_to, "/home/www/webdevelopercv.com"
 
 
 role :web, domain                          # Your HTTP server, Apache/etc
