fa743dd10519795c174cd7f4edb6980777bf8cfc
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2011-07-30 15:44:31 +0400

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper  
   def posted_on_message(date)
-    "Published on #{date.strftime("%B #{date.day.ordinalize}, %Y")}".html_safe
+     "Published on #{date.strftime("%B #{date.day.ordinalize}, %Y")}".html_safe
   end
 end
