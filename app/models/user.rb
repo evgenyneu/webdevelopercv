5e36bf34b715d843b4e38aac9cc8f099741cddce
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2012-04-07 19:23:46 +0600

    Updated to rails 3.2.3 and jQM 1.0.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User
        
   field :admin, type: Boolean, default: false
 
+  ## Database authenticatable
+  field :email,              :type => String, :null => false, :default => ""
+  field :encrypted_password, :type => String, :null => false, :default => ""
+
+  ## Recoverable
+  field :reset_password_token,   :type => String
+  field :reset_password_sent_at, :type => Time
+
+  ## Rememberable
+  field :remember_created_at, :type => Time
+
+  ## Trackable
+  field :sign_in_count,      :type => Integer, :default => 0
+  field :current_sign_in_at, :type => Time
+  field :last_sign_in_at,    :type => Time
+  field :current_sign_in_ip, :type => String
+  field :last_sign_in_ip,    :type => String
 end
