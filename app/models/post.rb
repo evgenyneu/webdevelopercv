b152898cd3ec4d23fd0b18e47fe139083ed00686
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2012-05-04 15:39:07 +0600

    Added sub titles field. Show subtitle on post show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,37 +1,38 @@
-class Post 
+class Post
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   field :title, type: String
+  field :sub_title, type: String
   field :permalink, type: String
   field :body, type: String
-  
+
   index :title, unique: true
   index :permalink, unique: true
 
   attr_writer :date
 
   def body_for_html(page_id)
-    if body_for_html_data.nil? 
+    if body_for_html_data.nil?
       body_for_html_data = self.body.gsub('***this-page-id***',page_id) unless self.body.nil?
       body_for_html_data ||= ""
     end
     body_for_html_data
   end
-  
+
   validates :body, :presence => true
-  
+
   before_save :before_saving_post
-  
-  def before_saving_post    
+
+  def before_saving_post
     return false unless parse_created_at
     self.permalink.strip!
-    self.permalink = self.title.z_to_url if self.permalink.empty?    
-    self.permalink = "/" + self.permalink if self.permalink[0] != "/"    
+    self.permalink = self.title.z_to_url if self.permalink.empty?
+    self.permalink = "/" + self.permalink if self.permalink[0] != "/"
   end
-  
+
   def self.by_permalink(permalink)
-    permalink = "/" + permalink if permalink[0] != "/"  
+    permalink = "/" + permalink if permalink[0] != "/"
     where(:permalink => permalink).first
   end
 
@@ -39,7 +40,7 @@ class Post
     order_by([[ :created_at, :desc ]])
   end
 
-  def date    
+  def date
     self.created_at.z_to_date_str unless self.created_at.nil?
   end
 
