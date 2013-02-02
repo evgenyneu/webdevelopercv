c8bfd1b148f67a381aebdcfc22c348064d404c29
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-02-03 00:49:42 +1100

    Deploy script change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :use_sudo, false
 default_run_options[:pty] = true
-default_environment['PATH'] = '/home/www/.rbenv/shims:/home/www/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin'
+default_environment['PATH'] = '/usr/bin/git:/home/www/.rbenv/shims:/home/www/.rbenv/bin'
 set :keep_releases, 5
 
 set :user, "www"
